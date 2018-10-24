350346599eea3d65280466609fa81790b1045348
xuhaixiang <xuhaixiangxhx@163.com>
2018-10-24 10:01:57 +0800

    tmp

--- apps/goods/admin.py
+++ apps/goods/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from goods.models import GoodsType
+from goods.models import GoodsType,GoodsSKU,Goods,GoodsImage,IndexGoodsBanner,IndexTypeGoodsBanner,IndexPromotionBanner
 # Register your models here.
 
 admin.site.register(GoodsType)
+admin.site.register(GoodsSKU)
+admin.site.register(Goods)
+admin.site.register(GoodsImage)
+admin.site.register(IndexGoodsBanner)
+admin.site.register(IndexTypeGoodsBanner)
+admin.site.register(IndexPromotionBanner)
