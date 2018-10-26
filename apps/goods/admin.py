19567cee99cefb45859992d1fcd75c5169db5d54
xuhaixiang <xuhaixiangxhx@163.com>
2018-10-26 16:15:43 +0800

    config static_index.html use celery

--- apps/goods/admin.py
+++ apps/goods/admin.py
@@ -1,11 +1,48 @@
 from django.contrib import admin
 from goods.models import GoodsType,GoodsSKU,Goods,GoodsImage,IndexGoodsBanner,IndexTypeGoodsBanner,IndexPromotionBanner
+from celery_tasks.tasks import generate_static_index_html
 # Register your models here.
 
-admin.site.register(GoodsType)
-admin.site.register(GoodsSKU)
-admin.site.register(Goods)
-admin.site.register(GoodsImage)
-admin.site.register(IndexGoodsBanner)
-admin.site.register(IndexTypeGoodsBanner)
-admin.site.register(IndexPromotionBanner)
+class BaseModelAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        '''新增或更新表中数据时调用'''
+        super().save_model(request, obj, form, change)
+        print('更新静态页面--start')
+        #更新静态页面
+        generate_static_index_html.delay()
+        print('更新静态页面--end')
+    def delete_model(self, request, obj):
+        '''删除表中数据时调用'''
+        super().delete_model(request, obj)
+        print('删除静态页面--start')
+        #更新静态页面
+        generate_static_index_html.delay()
+        print('删除静态页面--end')
+class GoodsTypeAdmin(BaseModelAdmin):
+    pass
+
+class GoodsSKUAdmin(BaseModelAdmin):
+    pass
+
+class GoodsAdmin(BaseModelAdmin):
+    pass
+
+class GoodsImageAdmin(BaseModelAdmin):
+    pass
+
+class IndexGoodsBannerAdmin(BaseModelAdmin):
+    pass
+
+class IndexTypeGoodsBannerAdmin(BaseModelAdmin):
+    pass
+
+class IndexPromotionBannerAdmin(BaseModelAdmin):
+    pass
+
+admin.site.register(GoodsType,GoodsTypeAdmin)
+admin.site.register(GoodsSKU,GoodsSKUAdmin)
+admin.site.register(Goods,GoodsAdmin)
+admin.site.register(GoodsImage,GoodsImageAdmin)
+admin.site.register(IndexGoodsBanner,IndexGoodsBannerAdmin)
+admin.site.register(IndexTypeGoodsBanner,IndexTypeGoodsBannerAdmin)
+admin.site.register(IndexPromotionBanner,IndexPromotionBannerAdmin)
--- celery_tasks/tasks.py
+++ celery_tasks/tasks.py
@@ -1,13 +1,16 @@
 from celery import Celery
 from django.conf import settings
 from django.core.mail import send_mail
-import time
+from django.template import loader
+import time,os
 
 # 在任务处理者(worker)一端加这几句,django环境初始化
 # import os
 # import django
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myshop.settings")
 # django.setup()
+
+from goods.models import GoodsType,IndexGoodsBanner,IndexPromotionBanner,IndexTypeGoodsBanner
 
 # 创建一个Celery类的实例对象
 app = Celery('celery_tasks.tasks',broker=settings.BROKER)
@@ -22,3 +25,47 @@ def send_register_active_email(to_email,username,token):
     html_msg = '%s,<br /><p style="text-indent:4em">欢迎成为myshop购物网站注册会员!请点击如下链接激活您的账户：</p><br /><a href="http://127.0.0.1:8000/user/active/%s">http://127.0.0.1:8000/user/active/%s</a>'%(username,token,token)
     send_mail(subject,message,sender,recive_list,html_message=html_msg)
     # time.sleep(5)
+
+@app.task
+def generate_static_index_html():
+    '''生成静态页面'''
+    print('生成静态页面--starrt')
+    #获取商品种类信息
+    types = GoodsType.objects.all()
+
+    #获取首页商品轮播信息
+    goods_banners = IndexGoodsBanner.objects.all().order_by('index')
+
+    #获取首页促销活动信息
+    promotion_banners = IndexPromotionBanner.objects.all().order_by('index')
+
+    #获取首页分类商品展示信息（包含图片分类，标题分类）
+    for type in types:
+        #获取标题分类商品信息
+        title_banners = IndexTypeGoodsBanner.objects.filter(type=type,display_type=0).order_by('index')
+        #获取图片分类商品信息
+        image_banners = IndexTypeGoodsBanner.objects.filter(type=type,display_type=1).order_by('index')
+
+        #动态给type添加属性
+        type.title_banners = title_banners
+        type.image_banners = image_banners
+
+    #组织模板上下文
+    context = {
+        'types':types,
+        'goods_banners':goods_banners,
+        'promotion_banners':promotion_banners,
+    }
+
+    # 使用模板
+    # 1.加载模板文件,返回模板对象
+    temp = loader.get_template('static_index.html')
+    # 2.模板渲染
+    static_index_html = temp.render(context)
+
+    index_file_path = os.path.join(settings.BASE_DIR,'static/static_index.html')
+    print('静态首页文件全路径',index_file_path)
+    with open(index_file_path,'wb') as f:
+        f.write(static_index_html)
+
+    print('生成静态页面--end')
